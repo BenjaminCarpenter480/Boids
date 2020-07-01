1362393de73f9da5abb4397bbc8013812867b481
Benjamin Carpenter <benjamincarpenter52@hotmail.co.uk>
2020-07-01 21:13:42 +0100

    Some basic working openGL code to be OOPed

---
+++ build-dir/CMakeFiles/HelloGL.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/benjamin/Desktop/SpaceObject/GLProj/main.cpp" "/home/benjamin/Desktop/SpaceObject/GLProj/build-dir/CMakeFiles/HelloGL.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../glfw-3.3.2/include"
+  "../glew-2.1.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/benjamin/Desktop/SpaceObject/GLProj/build-dir/glew-2.1.0/build/cmake/CMakeFiles/glew.dir/DependInfo.cmake"
+  "/home/benjamin/Desktop/SpaceObject/GLProj/build-dir/glfw-3.3.2/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-dir/CMakeFiles/HelloGL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloGL.dir/main.cpp.o"
+  "HelloGL.pdb"
+  "HelloGL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
