bcc9154dc360e8c3b130e060a31252ff7faf4df3
Benjamin Carpenter <benjamincarpenter52@hotmail.co.uk>
2020-08-06 18:20:34 +0100

    Added some improvment to the testing e,g, adding axis lines to the screen

--- main.cpp
+++ main.cpp
@@ -13,9 +13,9 @@
 
 
 int main()	{
-	const unsigned int particleCount=4; //IS actually a few less than this
+	const unsigned int particleCount=50; //IS actually a few less than this
 	const unsigned int dimensionCount=2;
-	const double dt_speed = 1;//Constant on the stepsize as movement is done on unit method
+	const double dt_speed = 100;//Constant on the stepsize as movement is done on unit method
 	const int k=100;//Density effectively (higher k lower density)
 	const float SLEEP_C= 0.95;//COnst on time to sleep to improve visibility
 	GLWrap test(particleCount,dimensionCount);
@@ -23,12 +23,24 @@ int main()	{
 			//             SPEDD,Konst
 	testSim testSimObj(particleCount,dt_speed,k);	
 	//testSimObj.timeStep();
+	//
+	int c =0;
 	while(!test.getExitAnimation())	{
 	    testSimObj.timeStep();
+	   
 	    //std::cout << testSimObj.getPosBuffer()<<std::endl;	    
-	    test.updateScreen(testSimObj.getPosBuffer());
-	    sleep(0.95);
+	    if (c%20==0)	{
+		test.screenClear();
+		test.draw(testSimObj.getBuffer());
+		test.updateScreen();
+	    }else{
+	    	testSimObj.getBuffer();
+	    std::cout<<"HERE\n";
+	    //sleep(0.95);
 	    //std::cin.get();
+	    }
+	c++;
+	std::cout <<c<<std::endl;
 	}
 	test.exitWindow();
 
